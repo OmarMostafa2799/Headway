6e8135ccd4a9e29526132cd8414baf4dad0da9d6
OmarMostafa2799 <128369809+OmarMostafa2799@users.noreply.github.com>
2025-02-02 21:23:35 +0200

    Update run_infrastructure.sh

--- run_infrastructure.sh
+++ run_infrastructure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+ 
 # Navigate to the Terraform directory and run Terraform commands
 echo "Running Terraform..."
-cd /home/ubuntu/Headway/terraform
+cd ./terraform
 terraform init   # Initialize Terraform
-terraform apply  # Apply the Terraform configuration
-
+terraform apply --auto-approve # Apply the Terraform configuration
+ 
 # Check if Terraform applied successfully
 if [ $? -eq 0 ]; then
     echo "Terraform ran successfully."
@@ -13,12 +13,14 @@ else
     echo "Terraform failed."
     exit 1
 fi
-
+ 
 # Navigate to the Ansible directory and run Ansible playbook
 echo "Running Ansible..."
-cd /home/ubuntu/Headway/ansible
-ansible-playbook -i inventory --private-key=key.pem jenkins.yml
-
+ 
+cd ../ansible/
+pwd
+ansible-playbook -i inventory --private-key key.pem jenkins.yml
+ 
 # Check if Ansible ran successfully
 if [ $? -eq 0 ]; then
     echo "Ansible ran successfully."
@@ -26,5 +28,6 @@ else
     echo "Ansible failed."
     exit 1
 fi
-
+ 
 echo "Both Terraform and Ansible ran successfully."
+ 
