8beb85c02310da733adceb0ffddec9644f5d29d5
OmarMostafa2799 <128369809+OmarMostafa2799@users.noreply.github.com>
2025-02-02 05:02:21 +0200

    Update 7-iam_role.tf

--- terraform/7-iam_role.tf
+++ terraform/7-iam_role.tf
@@ -113,7 +113,7 @@ resource "aws_iam_role_policy_attachment" "autoscaler" {
 
 resource "aws_iam_instance_profile" "worker" {
   depends_on = [aws_iam_role.worker]
-  name       = "ed-eks-worker-new-profile"
+  name       = "ed-eks-worker-new"
   role       = aws_iam_role.worker.name
 }
 
